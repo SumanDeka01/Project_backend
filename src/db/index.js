ece4b03133d2ca01993b6f33942f0d9930cc18b2
SumanDeka01 <sdeka2903@gmail.com>
2026-02-22 13:20:17 +0530

    MongoDB connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import e from "express";
+import { DB_NAME } from "../constants.js";
 
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-//         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("error: Connecting the database", error);
+    process.exit(1);
+  }
+};
 
-//     } catch (error) {
-//         console.log(error);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
+export default connectDB;
