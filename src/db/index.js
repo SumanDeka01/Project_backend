b08101459d4c9656d8f121d664aea03263f57df8
SumanDeka01 <sdeka2903@gmail.com>
2026-03-22 20:31:50 +0530

    Db fix

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGO_URI}/${DB_NAME}`
     );
-    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
-    console.log("error: Connecting the database", error);
+    console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   }
 };
