fa1c272373ab263fbfb83a9e4a84f82076452e78
SumanDeka01 <sdeka2903@gmail.com>
2026-03-23 23:57:31 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-// require("dotenv").config({ path: "./.env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-// import { connect } from "mongoose";
 import app from "./app.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
+
+console.log("Things are working");
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     });
   })
-  .catch((error) => {
-    console.log("Hmm Error Occured!", error);
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
   });
