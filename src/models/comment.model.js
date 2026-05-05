917fb10e4b7cdbdf0b49335a0dc8d9bf957f4a89
SumanDeka01 <sdeka2903@gmail.com>
2026-05-05 22:34:23 +0530

    Resolved merge conflict

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -3,10 +3,8 @@ import mongoose, { Schema } from "mongoose";
 const commentSchema = new Schema({
   content: {
     type: String,
-    required: false
+    required: false,
   },
-  
-  
 });
 
 export const Comment = mongoose.model("Comment", commentSchema);
